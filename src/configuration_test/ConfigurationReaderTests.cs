b763284f7f263d03c73c562f2e56286a5ad270fe
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2021-10-30 09:12:10 +0200

    moved tests to its own file

--- src/configuration_test/ConfigurationReaderTests.cs
+++ src/configuration_test/ConfigurationReaderTests.cs
@@ -7,14 +7,5 @@ namespace configuration_test
 {
     public class ConfigurationReaderTests
     {
-        [Fact]
-        public void should_read_and_validate_jsonschema()
-        {
-            var schemaJson = System.IO.File.ReadAllText("configuration_schema.json");
-            schemaJson.Should().NotBeNull();
-            
-            var schema = JSchema.Parse(schemaJson);
-            schema.Description.Should().Be("Schema for configuration.");
-        }
     }
 }
