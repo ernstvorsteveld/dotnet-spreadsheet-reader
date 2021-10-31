bb23541d98c4174f2d0a463b45d09eb8f09e9206
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2021-10-31 13:49:41 +0100

    moved test projects to test directory

---
+++ test/reader_test/FileStreamReaderTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace reader_test
+{
+    public class FileStreamReaderTests
+    {
+        [Fact]
+        public void should_read_spreadsheat()
+        {
+
+        }
+    }
+}
