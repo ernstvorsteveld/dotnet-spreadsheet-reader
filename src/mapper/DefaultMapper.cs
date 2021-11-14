d56e8671928b9ae491296cf1cd58bb72fb53efc4
Ernst Vorsteveld <ernst.vorsteveld@stern-itc.com>
2021-11-14 10:39:52 +0100

    added default mapper

---
+++ src/mapper/DefaultMapper.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace mapper
+{
+    public class DefaultMapper : IMapper
+    {
+        public IDictionary<string, object> Map(IDictionary<string, object> input)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/mapper/IMapper.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace mapper
+{
+    public interface IMapper
+    {
+        IDictionary<string,object> Map(IDictionary<string,object> input);
+    }
+}
